9189ec40589a2f9a797d33d93625e68394f74897
anderson <anderson@192.168.1.5>
2024-01-14 10:15:40 +0800

    project init

---
+++ MetaClone/MetaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaCloneApp.swift
+//  MetaClone
+//
+//  Created by anderson on 2024/1/14.
+//
+
+import SwiftUI
+
+@main
+struct MetaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
